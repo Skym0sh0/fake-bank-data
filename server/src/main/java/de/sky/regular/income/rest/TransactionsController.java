d328afdb571387d9e2a8645c4c314dfcb403fc47
Skym0sh0 <philipp_hannemann@gmx.de>
2019-06-01 22:13:10 +0200

    Added Update refinement

--- server/src/main/java/de/sky/regular/income/rest/TransactionsController.java
+++ server/src/main/java/de/sky/regular/income/rest/TransactionsController.java
@@ -5,13 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 @RestController
-@RequestMapping("/transaction")
+@RequestMapping("/transactions")
 public class TransactionsController {
     private static final Logger logger = LoggerFactory.getLogger(TransactionsController.class);
 
@@ -26,22 +29,24 @@ public class TransactionsController {
 
                     t.id = UUID.randomUUID();
 
-                    t.amountInCent = rnd.nextLong(-5000, 5000);
+                    t.amount = Math.round(((Math.random() * 10000) - 5000) * 100.0) / 100.0;
                     t.isPeriodic = rnd.nextBoolean();
-                    t.timestamp = LocalDate.now().minusDays(rnd.nextInt(1, 100));
+                    t.date = LocalDate.now().minusDays(rnd.nextInt(1, 100));
                     t.reason = "Reason " + i;
 
                     return t;
                 })
-                .sorted(Comparator.comparing(t -> t.timestamp))
+                .sorted(Comparator.comparing(t -> t.date))
                 .forEachOrdered(transactions::add);
     }
 
     @PostMapping
-    public Transaction createTransaction(@RequestBody Transaction t) {
+    public Transaction createTransaction(HttpServletResponse response, @RequestBody Transaction t) {
         logger.info("Create {}", t);
 
         transactions.add(t);
+
+        response.setStatus(HttpServletResponse.SC_CREATED);
 
         return t;
     }
@@ -64,18 +69,33 @@ public class TransactionsController {
     }
 
     @PatchMapping("/{id}")
-    public Transaction updateTransaction(@PathVariable UUID id, @RequestBody Transaction t) {
+    public Transaction updateTransaction(HttpServletResponse response, @PathVariable UUID id, @RequestBody Transaction t) {
         logger.info("Update {} with {}", id, t);
 
-        deleteTransaction(id);
+        Transaction old = this.transactions.stream()
+                .filter(tr -> Objects.equals(tr.id, id))
+                .findAny()
+                .orElseThrow();
 
-        return createTransaction(t);
+        updateIfSet(old, t, Transaction::getAmount, Transaction::setAmount);
+        updateIfSet(old, t, Transaction::getDate, Transaction::setDate);
+        updateIfSet(old, t, Transaction::getIsPeriodic, Transaction::setIsPeriodic);
+        updateIfSet(old, t, Transaction::getReason, Transaction::setReason);
+
+        return old;
+    }
+
+    private static <T> void updateIfSet(Transaction oldTxn, Transaction newTxn, Function<Transaction, T> getter, BiConsumer<Transaction, T> setter) {
+        Optional.ofNullable(getter.apply(newTxn))
+                .ifPresent(val -> setter.accept(oldTxn, val));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTransaction(@PathVariable("id") UUID id) {
+    public void deleteTransaction(HttpServletResponse response, @PathVariable("id") UUID id) {
         logger.info("Delete {}", id);
 
         transactions.removeIf(t -> Objects.equals(t.id, id));
+
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
     }
 }
