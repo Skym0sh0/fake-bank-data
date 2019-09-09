4364e3af4dc18484a2fe583c4c8c0a77641dac8f
Skym0sh0 <philipp_hannemann@gmx.de>
2019-09-09 17:42:18 +0200

    Save COmmit

--- server/server/src/main/java/de/sky/regular/income/rest/StatementController.java
+++ server/server/src/main/java/de/sky/regular/income/rest/StatementController.java
@@ -2,16 +2,23 @@ package de.sky.regular.income.rest;
 
 import de.sky.regular.income.api.Statement;
 import de.sky.regular.income.dao.StatementDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/statements")
 public class StatementController {
-    private final StatementDAO dao = new StatementDAO();
+    private final StatementDAO dao;
+
+    @Autowired
+    public StatementController(StatementDAO dao) {
+        this.dao = Objects.requireNonNull(dao);
+    }
 
     @GetMapping
     public List<Statement> getAllStatements() {
