edb72c1053174f1d8accd5e7c69eb26291fd8f0c
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2019-09-11 11:48:04 +0200

    Changed GUI to Statements Overview

--- gui/src/components/overview/StatementOverview.vue
+++ gui/src/components/overview/StatementOverview.vue
@@ -1,21 +1,63 @@
 <template>
-    <div v-if="false">
-        <h1>Balance Statements </h1>
-        <div class="statements">
-            <p v-bind:key="stmt.id" v-for="stmt in statements">{{stmt.date}} => {{stmt.balance}}</p>
-        </div>
-    </div>
+    <b-card>
+        <h1>Statement Overview</h1>
+
+        <b-btn @click="initiallyLoadData">Refresh</b-btn>
+
+        <b-table striped hover responsive="sm"
+                 id="bank-statements-table"
+                 primary-key="id"
+                 :fields="['index', 'date', 'balance', 'show_details']"
+                 :items="statements">
+            <template v-slot:cell(show_details)="row">
+                <b-button :id="`bank-statement-details-btn-${row.item.id}`"
+                          size="sm"
+                          class="mr-2"
+                          variant="info"
+                          @click="row.toggleDetails">
+                    {{row.detailsShowing ? 'Hide' : ' Show'}} Transactions
+                </b-button>
+            </template>
+
+            <template v-slot:row-details="row">
+                <statement-table-details :id="`bank-statement-details-${row.item.id}`"
+                                         :bank-statement="row.item"/>
+            </template>
+        </b-table>
+    </b-card>
 </template>
 
 <script>
+    import {api} from "../../api/RegularIncomeAPI";
+    import StatementTableDetails from "./StatementTableDetails";
+
     export default {
-        name: 'StatementOverview',
-        props: ["statements"]
+        name: "StatementOverview",
+        components: {StatementTableDetails},
+        data() {
+            return {
+                errorMessage: '',
+                selectedIndex: null,
+                statements: [],
+            }
+        },
+        methods: {
+            initiallyLoadData() {
+                api.getAllStatements()
+                    .then(res => {
+                        this.statements = res.data.sort((a, b) => b.date.localeCompare(a.date)).map((val, idx) => {
+                            val.index = idx
+                            return val
+                        })
+                    })
+                    .catch(e => this.errorMessage += e)
+            },
+        },
+        created() {
+            this.initiallyLoadData()
+        }
     }
 </script>
 
 <style scoped>
-    .statements p {
-        background: azure;
-    }
 </style>
---
+++ gui/src/components/overview/StatementTableDetails.vue
@@ -0,0 +1,40 @@
+<template>
+    <b-table :id="`statement-table-details-transations-table-${bankStatement.id}`"
+             :fields="['date', 'amount', 'isPeriodic', 'reason']"
+             :items="transactions">
+    </b-table>
+</template>
+
+<script>
+    import {api} from "../../api/RegularIncomeAPI";
+
+    export default {
+        name: "StatementTableDetails",
+        props: {
+            bankStatement: {
+                required: true,
+                type: Object,
+            }
+        },
+        data() {
+            return {
+                transactions: [],
+            }
+        },
+        methods: {
+            loadTransactions() {
+                api.getAllTransactions()
+                    .then(res => {
+                        this.transactions = res.data
+                    })
+            },
+        },
+        mounted() {
+            this.loadTransactions()
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
