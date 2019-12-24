cc4a0ca50335a6564b7be2472c685e9127f0ae95
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2019-12-24 09:22:20 +0100

    Added Annotation

--- server/server/src/main/java/de/sky/regular/income/Server.java
+++ server/server/src/main/java/de/sky/regular/income/Server.java
@@ -11,7 +11,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Server {
     @Bean
