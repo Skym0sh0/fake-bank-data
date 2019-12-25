1bf4e0d35bf5dbefb18509a73a8afac7dde0fe0d
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2019-12-25 11:31:14 +0100

    Rewrote Backup Scheduling

--- server/server/src/main/java/de/sky/regular/income/backup/BackupService.java
+++ server/server/src/main/java/de/sky/regular/income/backup/BackupService.java
@@ -1,39 +1,52 @@
 package de.sky.regular.income.backup;
 
 import org.slf4j.Logger;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Service;
 
+import java.time.*;
 import java.util.Objects;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-@EnableScheduling
-@Configuration
-@Controller
-public class BackupService {
+@Service
+public class BackupService implements InitializingBean {
     private static final Logger logger = getLogger(BackupService.class);
 
-    private static final String CRON_PART_SECOND = "0";
-    private static final String CRON_PART_MINUTE = "0";
-    private static final String CRON_PART_HOUR = "5";
-    private static final String CRON_PART_DAY_OF_MONTH = "*";
-    private static final String CRON_PART_MONTH = "*";
-    private static final String CRON_PART_DAY_OF_WEEK = "*";
-
-    private static final String CRON_FULL = CRON_PART_SECOND + " " + CRON_PART_MINUTE + " " + CRON_PART_HOUR + " " + CRON_PART_DAY_OF_MONTH + " " + CRON_PART_MONTH + " " + CRON_PART_DAY_OF_WEEK;
-
+    private final TaskScheduler scheduler;
     private final BackupSender sender;
 
-    public BackupService(BackupSender sender) {
+    @Value("${backup.process.time}")
+    private LocalTime backupTime;
+
+    public BackupService(TaskScheduler scheduler, BackupSender sender) {
+        this.scheduler = Objects.requireNonNull(scheduler);
         this.sender = Objects.requireNonNull(sender);
     }
 
-    @Scheduled(cron = CRON_FULL, zone = "UTC")
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Duration period = Duration.ofDays(1);
+
+        var now = ZonedDateTime.now(ZoneId.of("UTC"));
+        var scheduled = ZonedDateTime.of(LocalDate.now(), backupTime, now.getZone());
+
+        while (scheduled.isBefore(now))
+            scheduled = scheduled.plus(period);
+
+        scheduler.scheduleAtFixedRate(() -> {
+            doBackup();
+
+            logger.info("Next scheduled execution is in {} at {}", period, ZonedDateTime.now().plus(period));
+        }, scheduled.toInstant(), period);
+
+        logger.info("Scheduling set up successfully. Next execution is in {} at {}", Duration.between(now, scheduled), scheduled);
+    }
+
     public void doBackup() {
-        logger.info("Starting scheduled [{}] Backup...", CRON_FULL);
+        logger.info("Starting scheduled Backup...");
 
         try {
             sender.fetchDataAndBackup();
