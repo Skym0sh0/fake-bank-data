6c36d682f716068f8c7bdf229f453f9d0b89e39b
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2019-12-28 12:51:53 +0100

    Added MonetaryInput Component, but flawed

---
+++ gui/src/components/enterings/MonetaryInput.vue
@@ -0,0 +1,111 @@
+<template>
+    <div :id="id">
+        <b-form-input :id="`${id}-form-input`"
+                      v-model="rawInput"
+                      :disabled="disabled"
+                      :size="size"
+                      :placeholder="placeholder"
+                      :state="state"
+                      @focus="onFocus()"
+                      @blur="onFocusLost()"/>
+
+        <div v-if="false" class="debug">
+            <div>Id: {{id}}</div>
+            <div>RawInput: {{rawInput}}</div>
+            <div>Value: {{value}}</div>
+            <div>InCents: {{amountInCents()}}</div>
+            <div>Frmted: {{amountFormatted()}}</div>
+            <div>Decimal: {{amountInDecimal()}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {moneyFormat} from "../../util/Formatters";
+
+    export default {
+        name: "MonetaryInput",
+        props: {
+            id: {
+                required: true,
+                type: String,
+            },
+            value: {
+                required: true,
+                // type: Number,
+            },
+            state: {
+                required: false,
+            },
+            disabled: {
+                required: false,
+                type: Boolean,
+                default() {
+                    return false
+                }
+            },
+            placeholder: {
+                required: false,
+                type: String,
+            },
+            size: {
+                required: false,
+            },
+        },
+        data() {
+            return {
+                rawInput: null,
+                isFocused: false,
+            }
+        },
+        watch: {
+            value() {
+                this.rawInput = this.forRawInput()
+            },
+        },
+        methods: {
+            amountInCents() {
+                return this.value
+            },
+            amountInDecimal() {
+                return this.amountInCents() / 100.0
+            },
+            amountFormatted() {
+                return this.formatCentsToMoney(this.amountInDecimal())
+            },
+            forRawInput() {
+                if (!this.value && this.value !== 0)
+                    return null
+
+                if (!this.isFocused)
+                    return this.amountFormatted()
+
+                return this.amountInDecimal()
+            },
+            formatCentsToMoney(value) {
+                return moneyFormat.formatCents(value * 100)
+            },
+            onFocus() {
+                this.isFocused = true
+
+                this.rawInput = this.forRawInput()
+            },
+            onFocusLost() {
+                this.isFocused = false
+
+                const newCents = moneyFormat.parseToCents(this.rawInput)
+                this.$emit('input', newCents)
+            },
+        },
+        mounted() {
+            this.rawInput = this.forRawInput()
+        },
+    }
+</script>
+
+<style scoped>
+    .debug {
+        background-color: #999999;
+        color: lightblue;
+    }
+</style>
