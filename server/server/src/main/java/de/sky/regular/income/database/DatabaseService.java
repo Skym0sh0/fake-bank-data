965834c36959794bf8e53f62c36f89c755f707d4
Skym0sh0 <philipp_hannemann@gmx.de>
2020-01-05 16:16:07 +0100

    Minor changes

--- server/server/src/main/java/de/sky/regular/income/database/DatabaseService.java
+++ server/server/src/main/java/de/sky/regular/income/database/DatabaseService.java
@@ -1,13 +1,18 @@
 package de.sky.regular.income.database;
 
 import de.sky.common.database.DatabaseConnection;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @Service
 public class DatabaseService implements DatabaseSupplier, InitializingBean, DisposableBean {
+    private static final Logger logger = getLogger(DatabaseService.class);
+
     private DatabaseConnection connect;
 
     @Value("${config.jdbc.url}")
@@ -26,14 +31,24 @@ public class DatabaseService implements DatabaseSupplier, InitializingBean, Disp
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        logger.info("Connecting to Database");
+
         connect = ConnectDatabase.connect(url, user, password, schema);
 
+        logger.info("Database connected, now migrating Schema...");
+
         ConnectDatabase.migrateSchema(connect);
+
+        logger.info("Database connected and schema migrated successfully");
     }
 
     @Override
     public void destroy() throws Exception {
+        logger.info("Closing Database Connection...");
+
         if (connect != null)
             connect.close();
+
+        logger.info("Database Connection successfully closed");
     }
 }
