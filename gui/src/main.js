80c693f06fe2322f2c9d8a15ba00d30b4067ecea
Skym0sh0 <philipp_hannemann@gmx.de>
2020-01-06 10:42:57 +0100

    Added Vuetify

--- gui/src/main.js
+++ gui/src/main.js
@@ -6,6 +6,7 @@ import Vuelidate from 'vuelidate'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
@@ -14,5 +15,6 @@ Vue.use(Vuelidate)
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
