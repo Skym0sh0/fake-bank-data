7a1b941f53650d602b18b90b4e7b93271557bf57
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-01-03 21:30:59 +0100

    Upgraded Logging

--- server/database/build.gradle.kts
+++ server/database/build.gradle.kts
@@ -5,9 +5,8 @@ plugins {
 }
 
 //name = "database-commons"
-
 val jooq_version: String by rootProject.extra
-val logback_version: String by rootProject.extra
+val slf4j_version: String by rootProject.extra
 
 dependencies {
     api("org.jooq", "jooq", jooq_version)
@@ -16,16 +15,20 @@ dependencies {
 
     implementation("com.zaxxer", "HikariCP", "5.1.0")
 
-    implementation("org.slf4j", "slf4j-api", logback_version)
-    runtimeOnly("org.slf4j", "slf4j-simple", logback_version)
+    implementation("org.slf4j", "slf4j-api", slf4j_version)
+    runtimeOnly("org.slf4j", "slf4j-simple", slf4j_version)
 
     api("org.flywaydb", "flyway-core", "10.4.1")
     runtimeOnly("org.flywaydb", "flyway-database-postgresql", "10.4.1")
 
     implementation("org.postgresql", "postgresql", "42.7.1")
 
-    implementation("org.testcontainers", "testcontainers", "1.19.3")
-    implementation("org.testcontainers", "postgresql", "1.19.3")
+    implementation("org.testcontainers", "testcontainers", "1.19.3") {
+        exclude("org.slf4j", "slf4j-simple")
+    }
+    implementation("org.testcontainers", "postgresql", "1.19.3") {
+        exclude("org.slf4j", "slf4j-simple")
+    }
 }
 
 tasks {
