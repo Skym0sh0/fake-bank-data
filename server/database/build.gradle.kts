0442e555780aab54854dd4e794e7d89a6f634fb9
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-01-03 22:39:23 +0100

    Changed DB Sources Generator

--- server/database/build.gradle.kts
+++ server/database/build.gradle.kts
@@ -16,19 +16,15 @@ dependencies {
     implementation("com.zaxxer", "HikariCP", "5.1.0")
 
     implementation("org.slf4j", "slf4j-api", slf4j_version)
-    runtimeOnly("org.slf4j", "slf4j-simple", slf4j_version)
 
     api("org.flywaydb", "flyway-core", "10.4.1")
     runtimeOnly("org.flywaydb", "flyway-database-postgresql", "10.4.1")
 
     implementation("org.postgresql", "postgresql", "42.7.1")
 
-    implementation("org.testcontainers", "testcontainers", "1.19.3") {
-        exclude("org.slf4j", "slf4j-simple")
-    }
-    implementation("org.testcontainers", "postgresql", "1.19.3") {
-        exclude("org.slf4j", "slf4j-simple")
-    }
+    testImplementation("org.testcontainers", "testcontainers", "1.19.3")
+    testImplementation("org.testcontainers", "postgresql", "1.19.3")
+    testImplementation("org.slf4j", "slf4j-simple", slf4j_version)
 }
 
 tasks {
