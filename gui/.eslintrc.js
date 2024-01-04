8057afdd6ddae78a141f14f3d96404de44284112
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-01-04 09:54:37 +0100

    More Upgrades

---
+++ gui/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": [
+        'eslint:recommended',
+        'plugin:vue/recommended',
+        // 'plugin:vue/vue3-recommended',
+    ],
+    "rules": {
+        // 'vue/multi-word-component-names': 'off',
+    }
+}
