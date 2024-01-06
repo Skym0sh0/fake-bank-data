b199ae292eaca4569a3ee37f5cd0a1f5f5ba6795
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-01-06 13:39:38 +0100

    Visually improved User Avatar

--- gui/src/components/login/UserAvatar.vue
+++ gui/src/components/login/UserAvatar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="subtitle-1 d-flex justify-content-center align-items-baseline border rounded px-1" style="gap: 0.25em">
-        <v-avatar color="warning" size="24">
+        <v-avatar color="warning" size="32">
             {{ avatar }}
         </v-avatar>
         {{ username }}
@@ -15,10 +15,16 @@ export default {
             return this.$root.userRef.user.username;
         },
         avatar() {
-            return this.username.split(' ')
+            console.log(this.username.split(/\s+/)
                 .filter(word => word.length > 0)
-                .map(word => word[0])
-                .join("");
+                .map(word => word.substring(0, 1)))
+
+            return this.username.split(/\s+/)
+                .filter(word => word.length > 0)
+                .map(word => word.substring(0, 1))
+                .filter((letter, idx) => idx < 2)
+                .join("")
+                .toUpperCase();
         },
     },
 }
