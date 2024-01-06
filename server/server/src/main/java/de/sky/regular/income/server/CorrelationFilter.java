3da61386dfe64009c65589026be13edb62aa1111
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-01-06 20:37:45 +0100

    Minor Code Improvements

--- server/server/src/main/java/de/sky/regular/income/server/CorrelationFilter.java
+++ server/server/src/main/java/de/sky/regular/income/server/CorrelationFilter.java
@@ -1,39 +1,41 @@
 package de.sky.regular.income.server;
 
 import com.google.common.base.Stopwatch;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Component
-@Order(0)
-public class CorrelationFilter extends HttpFilter {
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorrelationFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (HttpMethod.OPTIONS.matches(request.getMethod())) {
-            super.doFilter(request, response, chain);
+            filterChain.doFilter(request, response);
             return;
         }
 
-        String correlationid = Optional.ofNullable(request.getHeader("correlationid"))
-                .orElse("unknown");
+        String correlationId = Optional.ofNullable(request.getHeader("correlationid"))
+                .orElseGet(() -> "generated-" + UUID.randomUUID());
 
-        log.info(" >>> {} {} - cor-id: {}", request.getMethod(), request.getServletPath(), correlationid);
+        log.info(" >>> {} {} - cor-id: {}", request.getMethod(), request.getServletPath(), correlationId);
         Stopwatch sw = Stopwatch.createStarted();
 
-        super.doFilter(request, response, chain);
+        filterChain.doFilter(request, response);
 
         sw.stop();
-        log.info(" <<< {} {} {} {} - cor-id: {}", request.getMethod(), request.getServletPath(), response.getStatus(), sw, correlationid);
+        log.info(" <<< {} {} {} {} - cor-id: {}", request.getMethod(), request.getServletPath(), response.getStatus(), sw, correlationId);
     }
 }
--- server/server/src/main/java/de/sky/regular/income/server/CorsHeaderFilter.java
+++ server/server/src/main/java/de/sky/regular/income/server/CorsHeaderFilter.java
@@ -1,22 +1,25 @@
 package de.sky.regular.income.server;
 
-import org.springframework.stereotype.Component;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import java.io.IOException;
 
 @Component
-public class CorsHeaderFilter extends HttpFilter {
+@Order(Ordered.HIGHEST_PRECEDENCE + 100)
+public class CorsHeaderFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, PUT, DELETE");
 
-        super.doFilter(request, response, chain);
+        filterChain.doFilter(request, response);
     }
 }
