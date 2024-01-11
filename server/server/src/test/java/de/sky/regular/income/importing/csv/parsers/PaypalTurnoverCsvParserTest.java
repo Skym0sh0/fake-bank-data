4937424afb7cb8e38fba94bbfcc6266f8fa81545
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-01-11 16:50:52 +0100

    Fixed Imports

--- server/server/src/test/java/de/sky/regular/income/importing/csv/parsers/PaypalTurnoverCsvParserTest.java
+++ server/server/src/test/java/de/sky/regular/income/importing/csv/parsers/PaypalTurnoverCsvParserTest.java
@@ -1,10 +1,6 @@
 package de.sky.regular.income.importing.csv.parsers;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
