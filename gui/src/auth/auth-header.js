c30de6dc76cd7129eb9a9af3c2aa635594b038fc
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-01-14 16:50:50 +0100

    Improved Login

--- gui/src/auth/auth-header.js
+++ gui/src/auth/auth-header.js
@@ -1,21 +1,39 @@
 const STORAGE_USER_KEY = "user";
 
+// const storage = sessionStorage;
+const storage = localStorage;
+
 const userReference = {
     user: null,
 }
 
+const storageAccess = {
+    store: (key, data) => {
+        storage.setItem(key, btoa(data))
+    },
+    read: (key) => {
+        const item = storage.getItem(key);
+        if (!item)
+            return null;
+        return atob(item)
+    },
+    delete: (key) => {
+        storage.removeItem(key)
+    },
+};
+
 function setUser(user) {
-    sessionStorage.setItem(STORAGE_USER_KEY, JSON.stringify(user))
+    storageAccess.store(STORAGE_USER_KEY, JSON.stringify(user))
     userReference.user = user;
 }
 
 function getUser() {
-    const userJson = sessionStorage.getItem(STORAGE_USER_KEY);
+    const userJson = storageAccess.read(STORAGE_USER_KEY);
     return JSON.parse(userJson)
 }
 
 function initUser() {
-    const userJson = sessionStorage.getItem(STORAGE_USER_KEY);
+    const userJson = storageAccess.read(STORAGE_USER_KEY);
     userReference.user = JSON.parse(userJson)
 }
 
@@ -24,7 +42,7 @@ function login(user, password) {
 }
 
 function logout() {
-    sessionStorage.removeItem(STORAGE_USER_KEY);
+    storageAccess.delete(STORAGE_USER_KEY);
     userReference.user = null;
 
     // location.reload();
@@ -37,10 +55,3 @@ export const userService = {
     logout,
     getUser,
 };
-
-/*
- TODO: mit axios verbinden
- -> bei auth error automatisch ausloggen
- -> auth header immer mitsenden
- */
-
