bb57f24413feb678ad0b7b320378c794f5dce920
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-01-18 18:03:29 +0100

    Deleted obsolete code

--- server/server/src/main/java/de/sky/regular/income/dao/ReportsDAO.java
+++ server/server/src/main/java/de/sky/regular/income/dao/ReportsDAO.java
@@ -20,7 +20,8 @@ import java.util.UUID;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
-import static generated.sky.regular.income.Tables.*;
+import static generated.sky.regular.income.Tables.TURNOVER_FILE_IMPORT;
+import static generated.sky.regular.income.Tables.TURNOVER_ROW;
 import static org.jooq.impl.DSL.*;
 
 @Service
@@ -124,76 +125,5 @@ public class ReportsDAO {
                         .sorted(Comparator.comparing(MonthlyIncomeExpenseReport.DataPoint::getMonth))
                         .toList()
         );
-    }
-
-    public StatementsReport doOldStatementsReport(DSLContext ctx, UUID userId, LocalDate begin, LocalDate end) {
-        var rprt = new StatementsReport();
-
-        rprt.data = ctx.select(BANK_STATEMENT.DATE_RECORD, BANK_STATEMENT.AMOUNT_BALANCE_CENTS)
-                .from(BANK_STATEMENT)
-                .where(and(
-                        BANK_STATEMENT.DATE_RECORD.ge(Optional.ofNullable(begin).orElse(LocalDate.of(1500, Month.JANUARY, 1))),
-                        BANK_STATEMENT.DATE_RECORD.lessThan(Optional.ofNullable(end).orElse(LocalDate.of(3000, Month.JANUARY, 1)))
-                ))
-                .and(BANK_STATEMENT.OWNER_ID.eq(userId))
-                .orderBy(BANK_STATEMENT.DATE_RECORD.asc())
-                .limit(MAX_NUMBER_RECORDS + 1)
-                .fetch()
-                .map(rec -> {
-                    var p = new StatementsReport.DataPoint();
-                    p.setDate(rec.get(BANK_STATEMENT.DATE_RECORD));
-                    p.setBalanceInCents(rec.get(BANK_STATEMENT.AMOUNT_BALANCE_CENTS));
-                    return p;
-                });
-
-        if (rprt.data.size() > MAX_NUMBER_RECORDS)
-            log.warn("Between [{}, {}) were more than {} datapoints", begin, end, MAX_NUMBER_RECORDS);
-
-        return rprt;
-    }
-
-    public MonthlyIncomeExpenseReport doMonthlyIncomeExpenseStatementReport(DSLContext ctx, UUID userId) {
-        var date = FINANCIAL_TRANSACTION.DATE_RECORD.as("date");
-        var amount = FINANCIAL_TRANSACTION.AMOUNT_VALUE_CENTS.as("amount");
-
-        var month = trunc(date, DatePart.MONTH);
-        var positiveSum = sum(when(amount.greaterOrEqual(0), amount).otherwise(0));
-        var negativeSum = sum(when(amount.lessThan(0), amount).otherwise(0));
-
-        MonthlyIncomeExpenseReport rprt = new MonthlyIncomeExpenseReport();
-
-        rprt.data = ctx.select(month, positiveSum, negativeSum)
-                .from(
-                        select(date, amount)
-                                .from(FINANCIAL_TRANSACTION)
-                                .where(FINANCIAL_TRANSACTION.OWNER_ID.eq(userId))
-                                .unionAll(
-                                        select(
-                                                BANK_STATEMENT.DATE_RECORD.as(date),
-                                                BANK_STATEMENT.AMOUNT_BALANCE_CENTS.as(amount)
-                                        )
-                                                .from(BANK_STATEMENT)
-                                                .where(BANK_STATEMENT.PREVIOUS_STATEMENT_ID.isNull())
-                                                .and(BANK_STATEMENT.OWNER_ID.eq(userId))
-                                )
-                )
-                .groupBy(month)
-                .orderBy(month)
-                .limit(MAX_NUMBER_RECORDS + 1)
-                .fetch()
-                .map(rec -> {
-                    var point = new MonthlyIncomeExpenseReport.DataPoint();
-
-                    point.month = rec.get(month);
-                    point.incomeInCents = rec.get(positiveSum).intValue();
-                    point.expenseInCents = -rec.get(negativeSum).intValue();
-
-                    return point;
-                });
-
-        if (rprt.data.size() > MAX_NUMBER_RECORDS)
-            log.warn("There are too many datapoints: {}", MAX_NUMBER_RECORDS);
-
-        return rprt;
     }
 }
