c3935220cfe02532ac231687eb03e60857f17339
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-06-29 14:08:06 +0200

    Added GUI Delivery for Spring Server

--- server/server/src/main/java/de/sky/regular/income/server/WebConfig.java
+++ server/server/src/main/java/de/sky/regular/income/server/WebConfig.java
@@ -1,8 +1,15 @@
 package de.sky.regular.income.server;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,5 +21,26 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("authorization", "content-type", "x-auth-token")
                 .exposedHeaders("x-auth-token");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new IndexFallbackResourceResolver());
+    }
+
+    static class IndexFallbackResourceResolver extends PathResourceResolver {
+        @Override
+        protected Resource resolveResourceInternal(HttpServletRequest request, String requestPath, List<? extends Resource> locations, ResourceResolverChain chain) {
+            // Give PathResourceResolver a chance to resolve a resource on its own.
+            Resource resource = super.resolveResourceInternal(request, requestPath, locations, chain);
+            if (resource == null) {
+                // If resource wasn't found, use index.html file.
+                resource = super.resolveResourceInternal(request, "index.html", locations, chain);
+            }
+            return resource;
+        }
     }
 }
