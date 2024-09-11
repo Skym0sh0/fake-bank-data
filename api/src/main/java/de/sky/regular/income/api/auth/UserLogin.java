f8cf733f7b5e91aa240412f310d6c123d44dffd9
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-09-11 15:17:19 +0200

    Removed obsolete Folder/Project

--- api/src/main/java/de/sky/regular/income/api/auth/UserLogin.java
+++
@@ -1,9 +0,0 @@
-package de.sky.regular.income.api.auth;
-
-import lombok.Data;
-
-@Data
-public class UserLogin {
-    public String username;
-    public String password;
-}
--- api/src/main/java/de/sky/regular/income/api/turnovers/TurnoverImportFormat.java
+++
@@ -1,8 +0,0 @@
-package de.sky.regular.income.api.turnovers;
-
-public enum TurnoverImportFormat {
-    VR_BANK_CSV,
-    DKB,
-    NEW_DKB,
-    PAYPAL
-}
