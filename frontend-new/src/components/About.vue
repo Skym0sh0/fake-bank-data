a6f6fa62eb9a0bbc759139dee83f63ba8d930b15
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-09-15 12:24:55 +0200

    feat: #64: Added About Page

---
+++ frontend-new/src/components/About.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This application is a banking app. You can track your in- and outcome here.</p>
+  </div>
+</template>
