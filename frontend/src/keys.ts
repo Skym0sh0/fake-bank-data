16479013c384df5002e7e0b021db16272066d17d
Skym0sh0 <4696583+Skym0sh0@users.noreply.github.com>
2024-10-14 16:25:39 +0200

    feat: Changed Provide/Inject Mechanism for Api to Pinia Store

--- frontend/src/keys.ts
+++ frontend/src/keys.ts
@@ -1,13 +1,4 @@
 import type {InjectionKey} from 'vue';
 import {ErrorNotifier} from "./auth/ErrorHandler.ts";
-import {CategoryApi, ReportsApi, TurnoversApi, UserAuthApi} from "@api/index.ts"
-
-export type ApiType = {
-  authApi: UserAuthApi;
-  reportsApi: ReportsApi;
-  categoriesApi: CategoryApi;
-  turnoversApi: TurnoversApi;
-}
 
 export const notifierRefKey = Symbol() as InjectionKey<ErrorNotifier>;
-export const apiRefKey = Symbol() as InjectionKey<ApiType>;
